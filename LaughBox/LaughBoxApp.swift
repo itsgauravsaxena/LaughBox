b7d45b062a5415cf952c6d457ee21f8dc4979abf
Gaurav Saxena <gauravsaxena.ec@gmail.com>
2026-04-04 06:18:42 +0200

    Initial Commit

---
+++ LaughBox/LaughBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaughBoxApp.swift
+//  LaughBox
+//
+//  Created by Gaurav Saxena on 04/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct LaughBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
